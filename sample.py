d345bc7ddb80c1fd92575e7db50b4fff69139351
Harsha Praneeth <praneethharsha10@gmail.com>
2020-03-10 15:12:33 +0100

    Update sample.py
    added an option to change blur intensity

--- sample.py
+++ sample.py
@@ -3,24 +3,25 @@ from PIL import Image
 image_url = 'jw.jpg'
 image_size = (300,300)
 image_mode = 'RGB'
+blurr_radius = 3
 
 image = Image.open(image_url).resize(image_size)
-m, n, pixels, blurred_pixels = image_size[0], image_size[1], list(image.getdata()), []
+m, n, br, dv, pixels, blurred_pixels = image_size[0], image_size[1], blurr_radius, (2*blurr_radius+1)**2, list(image.getdata()), []
 
 for i in range(m):
     for j in range(n):
-        try: a = pixels[i*m+j-1]
-        except: a = (0,0,0)
-        try: b = pixels[i*m+j+1]
-        except: b = (0,0,0)
-        try: c = pixels[(i-1)*m+j]
-        except: c = (0,0,0)
-        try: d = pixels[(i+1)*m+j]
-        except: d = (0,0,0)
-        blurred_pixels.append(((a[0]+b[0]+c[0]+d[0])//4, (a[1]+b[1]+c[1]+d[1])//4, (a[2]+b[2]+c[2]+d[2])//4))
+        r, g, b = 0, 0, 0
+        for x in range(-br,br+1):
+            for y in range(-br,br+1):
+                try:
+                    p = pixels[(i+x)*m+j+y]
+                    r, g, b = r+p[0], g+p[1], b+p[2]
+                except: pass
+        blurred_pixels.append((r//dv,g//dv,b//dv))
 
 
 blurred_image = Image.new(image_mode,image_size)
 blurred_image.putdata(blurred_pixels)
+blurred_image.save('blurred.jpg')
 image.show()
 blurred_image.show()
